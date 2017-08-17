6e11a2118600484c3eacf2d23d4f7dc25b269406
Kenton Duprey <kdtech18@gmail.com>
2017-08-17 00:23:47 -0400

    started GUI code
    Signed-off-by: Kenton Duprey <kdtech18@gmail.com>

--- src/GUI.java
+++ src/GUI.java
@@ -1,7 +1,15 @@
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class GUI {
 
+	JPanel mainPnl = new JPanel();
+	JPanel centerPnl = new JPanel();
+	JPanel southPnl = new JPanel();
+	JPanel eastPnl = new JPanel();
+	JPanel westPnl = new JPanel();
 
+	//TODO: Create layouts for dealer and player cards and info
+	//TODO: Create actions for when the player card is clicked
 }
